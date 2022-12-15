5caab3381abf8eb717543d25804e3a399f43c297
neurowelt <29918691+neurowelt@users.noreply.github.com>
2022-12-15 23:39:34 +0100

    Change paths to knowledge base

--- initialize.py
+++ initialize.py
@@ -3,5 +3,8 @@ import bot
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 OPR/93.0.0.0"
 
 chatbot = bot.ChatBot(
+    knowledge_base="./src/data/knowledge_base.pkl",
+    entity_base="./src/data/entity_base.pkl",
+    encoding_base="./src/data/encoding_base_comp.npz",
     user_agent=user_agent
 )
